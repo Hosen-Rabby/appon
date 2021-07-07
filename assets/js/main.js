10f2d420024d80341927a2f3dda3d3e928ee3e62
Hosen-Rabby <mdhosen21018@gmail.com>
2021-07-07 23:48:58 +0600

    Appon responsive website

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+// (function ($) {
+// 	"use strict";
+
+//     jQuery(document).ready(function($){
+
+
+//         $(".embed-responsive iframe").addClass("embed-responsive-item");
+//         $(".carousel-inner .item:first-child").addClass("active");
+        
+//         $('[data-toggle="tooltip"]').tooltip();
+
+
+//         $(".new-slider").owlCarousel({
+//         	    loop:true,
+// 			    margin:10,
+// 			    nav:true,
+// 			    items:1,
+//                 autoPlay:true,
+//                 navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+//                 dots: true,
+//         });
+
+        
+
+
+//     });
+
+
+//     jQuery(window).load(function(){
+
+        
+//     });
+
+
+// }(jQuery));	
+
+
+
